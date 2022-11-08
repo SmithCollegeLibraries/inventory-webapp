f6d699d279abe44551259f8bdc456d8bfe878ecc
Micah Walter <micahjwalter@gmail.com>
2022-11-08 16:02:34 -0500

    Finished user management tab

--- src/containers/userManagement.js
+++ src/containers/userManagement.js
@@ -13,10 +13,11 @@ import Alerts from '../components/alerts';
 import classnames from 'classnames';
 import { CreateAccount } from '../components/login';
 
-function UserManagement(){
+function UserManagement() {
   const [users, setUsers] = useState([]);
   const [level, setLevel] = useState(0);
-  const [activeTab, setActiveTab ] = useState('user_update');
+  const [password, setPassword] = useState("");
+  const [activeTab, setActiveTab ] = useState("user_update");
 
   useEffect(() => {
     getUsers();
@@ -35,14 +36,16 @@ function UserManagement(){
   };
 
   const handleSubmit = async (e, index) => {
+    e.preventDefault();
     const data = users[index];
     const update = await Load.accountUpdate(data);
+    users[index].password = "";
+    setPassword("");
     if (update) {
-      Alerts.success('Account successfully updated');
-      getUsers();
+      Alerts.success("Account successfully updated");
     } else {
       const errors = {
-        name: 'Error updating account',
+        name: "Error updating account",
         message: "There was an error updating this account",
       };
       Alerts.error(errors);
@@ -51,16 +54,14 @@ function UserManagement(){
 
   const updateLevel = (e, index) => {
     e.preventDefault();
-    const user = users;
-    user[index].level = e.target.value;
-    setUsers(user);
+    users[index].level = e.target.value;
     setLevel(e.target.value);
   };
 
   const updatePassword = (e, index) => {
-    const user = users;
-    user[index].password = e.target.value;
-    setUsers(user);
+    e.preventDefault();
+    users[index].password = e.target.value;
+    setPassword(e.target.value);
   };
 
   const handleDelete = (e, index) => {
@@ -136,7 +137,7 @@ const UserUpdate = ({ users, updatePassword, updateLevel, handleSubmit, handleDe
           <tr key={index}>
             <td>{users[items].name}</td>
             <td>{users[items].email}</td>
-            <td><Input type="password" name="password" onChange={e => updatePassword(e, index)} /></td>
+            <td><Input type="password" name="password" value={users[items].password} onChange={e => updatePassword(e, index)} /></td>
             <td><Input type="number" name="level" value={users[items].level} min="0" max="100" onChange={e => updateLevel(e, index)} /></td>
             <td><Button color="primary" onClick={(e) => handleSubmit(e, index)}>Update</Button></td>
             <td><Button color="danger" onClick={(e) => handleDelete(e, index)}>Delete</Button></td>
@@ -149,81 +150,81 @@ const UserUpdate = ({ users, updatePassword, updateLevel, handleSubmit, handleDe
 
 const UserCreation = () => {
 
-    const initialState = {
-      createNewAccount : {
-        email: '',
-        password: '',
-        name: '',
-        level: '',
-      }
+  const initialState = {
+    createNewAccount : {
+      email: '',
+      password: '',
+      name: '',
+      level: '',
+    }
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'CREATE_ACCOUNT':
+        const createNewAccount = state.createNewAccount;
+        createNewAccount[action.payload.name] = action.payload.value;
+        return {
+          ...state,
+          createNewAccount,
+        };
+      case 'RESET':
+        return {
+          ...state,
+          createNewAccount: {
+            email: '',
+            password: '',
+            name: '',
+            level: '',
+          }
+        };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const handleCreationChange = e => {
+    dispatch({
+      type: 'CREATE_ACCOUNT',
+      payload: {
+        name: e.target.name,
+        value: e.target.value,
+      }});
+  };
+
+  const handleAccountCreationSubmit = async e => {
+    e.preventDefault();
+    const data = {
+      email: state.createNewAccount.email,
     };
-
-    const reducer = (state, action) => {
-      switch (action.type) {
-        case 'CREATE_ACCOUNT':
-          const createNewAccount = state.createNewAccount;
-          createNewAccount[action.payload.name] = action.payload.value;
-          return {
-            ...state,
-            createNewAccount,
-          };
-        case 'RESET':
-          return {
-            ...state,
-            createNewAccount: {
-              email: '',
-              password: '',
-              name: '',
-              level: '',
-            }
-          };
-        default:
-          throw new Error();
-      }
-    };
-
-    const [ state, dispatch ] = useReducer(reducer, initialState);
-
-    const handleCreationChange = e => {
-      dispatch({
-        type: 'CREATE_ACCOUNT',
-        payload: {
-          name: e.target.name,
-          value: e.target.value,
-        }});
-    };
-
-    const handleAccountCreationSubmit = async e => {
-      e.preventDefault();
-      const data = {
-        email: state.createNewAccount.email,
-      };
-      const account = await Load.verifyAccount(data);
-      if (account) {
-        Alerts.success('This account already exists');
+    const account = await Load.verifyAccount(data);
+    if (account) {
+      Alerts.success('This account already exists');
+    } else {
+      const create = await Load.createAccount(state.createNewAccount);
+      if (create) {
+        dispatch({ type: 'RESET', payload: {}});
+        Alerts.success('Account successfully created');
       } else {
-        const create = await Load.createAccount(state.createNewAccount);
-        if (create) {
-          dispatch({ type: 'RESET', payload: {}});
-          Alerts.success('Account successfully created');
-        } else {
-          const error = {
-            name: 'Account creation',
-            message: "There was a problem creating your account.",
-          };
-          Alerts.error(error);
-        }
+        const error = {
+          name: 'Account creation',
+          message: "There was a problem creating your account.",
+        };
+        Alerts.error(error);
       }
-    };
+    }
+  };
 
-    return (
-      <div>
-        <CreateAccount
-          handleAccountCreationSubmit={handleAccountCreationSubmit}
-          handleCreationChange={handleCreationChange}
-        />
-      </div>
-    );
+  return (
+    <div>
+      <CreateAccount
+        handleAccountCreationSubmit={handleAccountCreationSubmit}
+        handleCreationChange={handleCreationChange}
+      />
+    </div>
+  );
 }
 
 export default UserManagement;
