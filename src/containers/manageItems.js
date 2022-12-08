17020a1337d78ec70b8086bfcc12257f6c2c9a2f
Micah Walter <micahjwalter@gmail.com>
2022-12-08 13:43:58 -0500

    Show tray barcode and collection name with item list

--- src/containers/manageItems.js
+++ src/containers/manageItems.js
@@ -1,7 +1,6 @@
 import React, { useReducer, useEffect } from 'react';
 import Load from '../util/load';
-import { success, failure } from '../components/toastAlerts';
-import { Row, Col, Form, Button, Input, Card, CardBody } from 'reactstrap';
+import { Row, Col, Table, Button, Input, Card, CardBody } from 'reactstrap';
 
 const itemReducer = (state, action) => {
   switch (action.type) {
@@ -42,119 +41,27 @@ function ManageItems(props) {
     dispatch({ type: "ADD_ITEMS", items: items});
   };
 
-  const handleFormChange = (e) => {
-    dispatch({ type: "UPDATE_ITEM_FORM", items: e.target.value});
-  };
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    const createData = {
-      'barcode' : data.item
-    };
-    const results = await Load.createNewItem(createData);
-    if (results) {
-      success("New item successfully created");
-      dispatch({ type: "UPDATE_ITEM_FORM", items: ""});
-      items();
-      props.newItems();
-    } else {
-      failure("Unable to create item");
-    }
-  };
-
-  const handleUpdateFormChange = (e, key) => {
-    const item = data.items;
-    item[key]["barcode"] = e.target.value;
-    dispatch({ type: "UPDATE_ITEMS", items: item});
-  };
-
-  const handleUpdateSubmit = async(e, key) => {
-    const update = await Load.updateItem(data.items[key]);
-    if (update) {
-      success('Items updated');
-    } else {
-      failure("There was a problem updating this item");
-    }
-    items();
-    props.newItems();
-  };
-
-  const handleDeleteSubmit = async(e, data) => {
-    e.preventDefault();
-    const hasItems = await Load.itemHasItems(data);
-    if (hasItems) {
-      failure("This item cannot be deleted because it has items tied to it");
-    } else {
-      if (window.confirm("Delete this item? This action can only be undone by the database administrator.")) {
-        const set = await Load.deleteItem({id: data.id});
-        if (set) {
-          success('Item removed');
-        } else {
-          failure('Item could not be removed');
-        }
-      }
-    }
-    items();
-    props.newItems();
-  };
-
   return (
-    <div>
-      <div style={{backgroundColor: "#fff", padding: '20px', textAlign: "middle", marginTop: "20px"}}>
-        <DisplayForm
-          handleFormChange={handleFormChange}
-          handleFormSubmit={handleFormSubmit}
-          item={data.item}
-        />
-      </div>
-      <Card>
-        <CardBody>
-          {Object.keys(data.items).map((items, idx) => {
-            return (
-              <Display
-                data={data.items[items]}
-                index={idx}
-                key={idx}
-                handleUpdateSubmit={handleUpdateSubmit}
-                handleUpdateFormChange={handleUpdateFormChange}
-                handleDeleteSubmit={handleDeleteSubmit}
-              />
-            );
-          })}
-        </CardBody>
-      </Card>
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Barcode</th>
+          <th>Tray</th>
+          <th>Collection</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.items && data.items.length ? Object.keys(data.items).map((key, index) =>
+          <tr key={index}>
+            <td>{data.items[key].barcode}</td>
+            <td>{data.items[key].tray}</td>
+            <td>{data.items[key].collection}</td>
+          </tr>
+        ) : null}
+      </tbody>
+    </Table>
   );
 
 }
 
 export default ManageItems;
-
-const DisplayForm = ({ handleFormChange, handleFormSubmit, item }) => (
-  <Form onSubmit={(e) => handleFormSubmit(e)}>
-    <Row>
-      <Col md="8">
-        <Input type="text" value={item} onChange={(e) => handleFormChange(e)} name="item" placeholder="Add a new item..." />
-      </Col>
-      <Col md="2">
-        <Button color="primary">Submit</Button>
-      </Col>
-    </Row>
-  </Form>
-);
-
-const Display = ({ data, index, handleUpdateFormChange, handleUpdateSubmit, handleDeleteSubmit }) => {
-  return (
-    <div key={index} style={{paddingBottom: "20px"}}>
-      <Row>
-        <Col md="8">
-          <Input type="text" onChange={(e) => handleUpdateFormChange(e, index)} value={data.barcode} name="barcode" />
-        </Col>
-        <Col>
-          {/* <Button color="primary" style={{"marginRight": "10px"}} onClick={(e) => {handleUpdateSubmit(e, index)}}>Update</Button>
-          <Button color="danger" onClick={(e) => {handleDeleteSubmit(e, data)}}>Delete</Button> */}
-        </Col>
-      </Row>
-    </div>
-  )
-};
