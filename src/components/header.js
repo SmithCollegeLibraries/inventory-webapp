68d87028386ad061ace63777dd66349f5f06d566
Micah Walter <micahjwalter@gmail.com>
2023-03-06 12:49:33 -0500

    Update version number

--- src/components/header.js
+++ src/components/header.js
@@ -40,7 +40,7 @@ export default class Header extends Component {
     return (
       <div>
         <Navbar color="dark" dark expand="md">
-          <NavbarBrand href="#!">SIS &rsquo;23 (Version 4.1.7)</NavbarBrand>
+          <NavbarBrand href="#!">SIS &rsquo;23 (Version 4.1.8)</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           {sessionStorage.getItem('account') ?
             <Collapse isOpen={this.state.isOpen} navbar>
