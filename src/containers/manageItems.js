c74f27abe3607a74cc295fa7db936f14d578ee52
Micah Walter <micahjwalter@gmail.com>
2023-08-01 17:35:10 -0400

    Add "To return to campus" status

--- src/containers/manageItems.js
+++ src/containers/manageItems.js
@@ -452,6 +452,7 @@ const ItemForm = (props) => {
                   <option value={null}>(none)</option>
                   <option value="Trayed">Trayed</option>
                   <option value="Circulating">Circulating</option>
+                  <option value="To return to campus">To return to campus</option>
                   <option value="Returned to campus">Returned to campus</option>
                   <option value="Missing">Missing</option>
                 </Input>
