60ae858727087992cd54399b3ccdee00aed4ad74
Micah Walter <micahjwalter@gmail.com>
2025-02-19 14:11:02 -0500

    Add tray and item counts

--- src/containers/reportCounts.js
+++ src/containers/reportCounts.js
@@ -85,17 +85,14 @@ const ReportCounts = () => {
   useEffect(() => {
     async function fetchTrayCount() {
       // Get the ladder of count in order to set shelves per ladder
-      const ladderTotal = await Load.ladderCount();
       const shelfTotal = await Load.shelfCount();
+      useCounts.setState({ shelfTotal });
+      const ladderTotal = await Load.ladderCount();
+      useCounts.setState({ shelvesPerLadder: shelfTotal / ladderTotal });
       const trayTotal = await Load.trayCount();
+      useCounts.setState({ trayTotal });
       const itemTotal = await Load.itemCount();
-      useCounts.setState((prevState) => ({
-        ...prevState,
-        shelfTotal: shelfTotal,
-        trayTotal: trayTotal,
-        itemTotal: itemTotal,
-        shelvesPerLadder: shelfTotal / ladderTotal,
-      }));
+      useCounts.setState({ itemTotal });
     }
     fetchTrayCount();
   }, []);
@@ -103,48 +100,88 @@ const ReportCounts = () => {
   // Get the count of each size in each collection on load and store the
   // count of each size in each collection in a 2D array
   useEffect(() => {
-    async function fetchShelfSubtotals() {
+    async function fetchSubtotals() {
       let allCollections = await Load.getAllCollections();
       let allSizes = await Load.getAllSizes();
+
+      const shelfApiBreakdown = await Load.shelfCountsCollectionSize();
+
       // Add null and total collection and size
       allCollections.push({ code: null });
       allCollections.unshift({ code: ALL_COLLECTIONS });
       allSizes.push({ code: null });
       allSizes.unshift({ code: ALL_SIZES });
-      useCounts.setState({ allCollections });
-      useCounts.setState({ allSizes });
+      useCounts.setState({ allCollections, allSizes });
 
-      const subtotalsFromApi = await Load.shelfCountsCollectionSize();
       // Initialize an empty 2D array of shelf subtotals by collection and size
       let shelfSubtotals = {};
+      let traySubtotals = {};
+      let itemSubtotals = {};
       // Add a row for each collection
       for (let i = 0; i < allCollections.length; i++) {
         shelfSubtotals[allCollections[i].code] = {};
+        traySubtotals[allCollections[i].code] = {};
+        itemSubtotals[allCollections[i].code] = {};
         // Add a column for each size
         for (let j = 0; j < allSizes.length; j++) {
           shelfSubtotals[allCollections[i].code][allSizes[j].code] = 0;
+          traySubtotals[allCollections[i].code][allSizes[j].code] = 0;
+          itemSubtotals[allCollections[i].code][allSizes[j].code] = 0;
         }
       }
       // Go through each subtotal and set it in the state. In addition,
       // have the "total" size and collection be the sum of all the other
       // sizes and collections, including any nulls that don't exist in
       // any of the subtotals
-      for (let i = 0; i < subtotalsFromApi.length; i++) {
+      for (let i = 0; i < shelfApiBreakdown.length; i++) {
         // If the collection is active, assign it to the relevant collection
-        if (shelfSubtotals[subtotalsFromApi[i].collection_code]) {
-          shelfSubtotals[subtotalsFromApi[i].collection_code][subtotalsFromApi[i].size] = subtotalsFromApi[i].count;
-          shelfSubtotals[subtotalsFromApi[i].collection_code][ALL_SIZES] += subtotalsFromApi[i].count;
+        if (shelfSubtotals[shelfApiBreakdown[i].collection_code]) {
+          shelfSubtotals[shelfApiBreakdown[i].collection_code][shelfApiBreakdown[i].size] = shelfApiBreakdown[i].count;
+          shelfSubtotals[shelfApiBreakdown[i].collection_code][ALL_SIZES] += shelfApiBreakdown[i].count;
         }
         // Otherwise, add it to the null/unassigned collection
         else {
-          shelfSubtotals[null][subtotalsFromApi[i].size] = subtotalsFromApi[i].count;
-          shelfSubtotals[null][ALL_SIZES] += subtotalsFromApi[i].count;
+          shelfSubtotals[null][shelfApiBreakdown[i].size] = shelfApiBreakdown[i].count;
+          shelfSubtotals[null][ALL_SIZES] += shelfApiBreakdown[i].count;
         }
-        shelfSubtotals[ALL_COLLECTIONS][subtotalsFromApi[i].size] += subtotalsFromApi[i].count;
+        shelfSubtotals[ALL_COLLECTIONS][shelfApiBreakdown[i].size] += shelfApiBreakdown[i].count;
       }
       useCounts.setState({ shelfSubtotals });
+
+      const trayApiBreakdown = await Load.trayCountsCollectionSize();
+      for (let i = 0; i < trayApiBreakdown.length; i++) {
+        // If the collection is active, assign it to the relevant collection
+        if (traySubtotals[trayApiBreakdown[i].collection_code]) {
+          traySubtotals[trayApiBreakdown[i].collection_code][trayApiBreakdown[i].size] = trayApiBreakdown[i].count;
+          traySubtotals[trayApiBreakdown[i].collection_code][ALL_SIZES] += trayApiBreakdown[i].count;
+        }
+        // Otherwise, add it to the null/unassigned collection
+        else {
+          traySubtotals[null][trayApiBreakdown[i].size] = trayApiBreakdown[i].count;
+          traySubtotals[null][ALL_SIZES] += trayApiBreakdown[i].count;
+        }
+        traySubtotals[ALL_COLLECTIONS][trayApiBreakdown[i].size] += trayApiBreakdown[i].count;
+      }
+      useCounts.setState({ traySubtotals });
+
+      const itemApiBreakdown = await Load.itemCountsCollectionSize();
+      for (let i = 0; i < itemApiBreakdown.length; i++) {
+        // If the collection is active, assign it to the relevant collection
+        if (itemSubtotals[itemApiBreakdown[i].collection_code]) {
+          itemSubtotals[itemApiBreakdown[i].collection_code][itemApiBreakdown[i].size] = itemApiBreakdown[i].count;
+          itemSubtotals[itemApiBreakdown[i].collection_code][ALL_SIZES] += itemApiBreakdown[i].count;
+        }
+        // Otherwise, add it to the null/unassigned collection
+        else {
+          itemSubtotals[null][itemApiBreakdown[i].size] = itemApiBreakdown[i].count;
+          itemSubtotals[null][ALL_SIZES] += itemApiBreakdown[i].count;
+        }
+        itemSubtotals[ALL_COLLECTIONS][itemApiBreakdown[i].size] += itemApiBreakdown[i].count;
+      }
+      useCounts.setState({ itemSubtotals });
     }
-    fetchShelfSubtotals();
+
+    fetchSubtotals();
   }, []);
 
   return (
@@ -162,36 +199,147 @@ const ReportCounts = () => {
             {view}
           </Button>
         ))}
-        <Button color="info" onClick={() => {navigator.clipboard.writeText(state.totalCountText(state, currentView))}} style={{"cursor": "grab", "marginLeft": "auto"}}>{`${state.totalCountText(state, currentView)} total`}</Button>
+        <Button color="info" onClick={() => {navigator.clipboard.writeText(state.totalCountText(currentView))}} style={{"cursor": "grab", "marginLeft": "auto"}}>{`${state.totalCountText(currentView)} total`}</Button>
       </Row>
-      <ShelfCounts
-        shelfTotal={state.shelfTotal}
-        shelfSubtotals={state.shelfSubtotals}
-        allCollections={allCollections}
-        allSizes={allSizes}
-        shelvesPerLadder={state.shelvesPerLadder}
-        inLadders={currentView === LADDERS}
-      />
+      { currentView === ITEMS
+        ? <ItemCounts
+          itemTotal={state.itemTotal}
+          itemSubtotals={state.itemSubtotals}
+          allCollections={allCollections}
+          allSizes={allSizes}
+        />
+        : (
+          currentView === TRAYS
+          ? <TrayCounts
+            trayTotal={state.trayTotal}
+            traySubtotals={state.traySubtotals}
+            allCollections={allCollections}
+            allSizes={allSizes}
+          />
+          : <ShelfCounts
+              shelfTotal={state.shelfTotal}
+              shelfSubtotals={state.shelfSubtotals}
+              allCollections={allCollections}
+              allSizes={allSizes}
+              shelvesPerLadder={state.shelvesPerLadder}
+              inLadders={currentView === LADDERS}
+            />
+        )
+      }
     </div>
   );
 };
 
+const ItemCounts = (props) => {
+  return (
+    <Table style={{tableLayout: "fixed"}}>
+      <thead>
+        <tr>
+          <th style={{width: "8em", textAlign: "right"}}></th>
+          { Object.keys(props.allSizes).map((sizeIndex) => (
+              <th key={`header-size-${sizeIndex}`} style={{width: `${100/props.allSizes.length}%`, textAlign: "right"}}>{props.allSizes[sizeIndex].code ?? UNASSIGNED_SIZE }</th>
+          )) }
+        </tr>
+      </thead>
+      <tbody>
+        { Object.keys(props.allCollections).map((collectionIndex) =>
+          <tr key={`row-collection-${collectionIndex}`}>
+            <th key={`row-collection-${collectionIndex}`}>{props.allCollections[collectionIndex].code ?? UNASSIGNED_COLLECTION }</th>
+            { Object.keys(props.allSizes).map((sizeIndex) => (
+              <td key={`cell-${collectionIndex}-${sizeIndex}`}
+                  style={{
+                    textAlign: "right",
+                    color: props.allCollections[collectionIndex].code === ALL_COLLECTIONS || props.allSizes[sizeIndex].code === ALL_SIZES
+                      ? "#0d6efd"
+                      : (
+                        !props.itemSubtotals[props.allCollections[collectionIndex].code] || props.itemSubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code] === 0
+                        ? "#e9ecef"
+                        : "black"
+                      )
+                  }}
+              >
+                { props.allCollections[collectionIndex].code === ALL_COLLECTIONS && props.allSizes[sizeIndex].code === ALL_SIZES
+                  ? props.itemTotal.toLocaleString('en-US')
+                  : (
+                    props.itemSubtotals[props.allCollections[collectionIndex].code] && props.itemSubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code]
+                      ? props.itemSubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code].toLocaleString('en-US')
+                      : 0
+                  )
+                }
+              </td>
+            ))}
+          </tr>
+          )
+        }
+      </tbody>
+    </Table>
+  );
+};
+
+const TrayCounts = (props) => {
+  return (
+    <Table style={{tableLayout: "fixed"}}>
+      <thead>
+        <tr>
+          <th style={{width: "8em", textAlign: "right"}}></th>
+          { Object.keys(props.allSizes).map((sizeIndex) => (
+              <th key={`header-size-${sizeIndex}`} style={{width: `${100/props.allSizes.length}%`, textAlign: "right"}}>{props.allSizes[sizeIndex].code ?? UNASSIGNED_SIZE }</th>
+          )) }
+        </tr>
+      </thead>
+      <tbody>
+        { Object.keys(props.allCollections).map((collectionIndex) =>
+          <tr key={`row-collection-${collectionIndex}`}>
+            <th key={`row-collection-${collectionIndex}`}>{props.allCollections[collectionIndex].code ?? UNASSIGNED_COLLECTION }</th>
+            { Object.keys(props.allSizes).map((sizeIndex) => (
+              <td key={`cell-${collectionIndex}-${sizeIndex}`}
+                  style={{
+                    textAlign: "right",
+                    color: props.allCollections[collectionIndex].code === ALL_COLLECTIONS || props.allSizes[sizeIndex].code === ALL_SIZES
+                      ? "#0d6efd"
+                      : (
+                        !props.traySubtotals[props.allCollections[collectionIndex].code] || props.traySubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code] === 0
+                        ? "#e9ecef"
+                        : "black"
+                      )
+                  }}
+              >
+                { props.allCollections[collectionIndex].code === ALL_COLLECTIONS && props.allSizes[sizeIndex].code === ALL_SIZES
+                  ? props.trayTotal.toLocaleString('en-US')
+                  : (
+                    props.traySubtotals[props.allCollections[collectionIndex].code] && props.traySubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code]
+                      ? props.traySubtotals[props.allCollections[collectionIndex].code][props.allSizes[sizeIndex].code].toLocaleString('en-US')
+                      : 0
+                  )
+                }
+              </td>
+            ))}
+          </tr>
+          )
+        }
+      </tbody>
+    </Table>
+  );
+};
+
+// ShelfCounts is used for ladder counts as well, which just use a fixed
+// conversion ratio
 const ShelfCounts = (props) => {
   return (
     <Table style={{tableLayout: "fixed"}}>
       <thead>
         <tr>
           <th style={{width: "8em", textAlign: "right"}}></th>
-          {Object.keys(props.allSizes).map((sizeIndex) => (
-            <th key={`header-size-${sizeIndex}`} style={{width: `${100/props.allSizes.length}%`, textAlign: "right"}}>{props.allSizes[sizeIndex].code ?? UNASSIGNED_SIZE }</th>
-          ))}
+          { Object.keys(props.allSizes).map((sizeIndex) => (
+              <th key={`header-size-${sizeIndex}`} style={{width: `${100/props.allSizes.length}%`, textAlign: "right"}}>{props.allSizes[sizeIndex].code ?? UNASSIGNED_SIZE }</th>
+          )) }
         </tr>
       </thead>
       <tbody>
-        {Object.keys(props.allCollections).map((collectionIndex) =>
+        { Object.keys(props.allCollections).map((collectionIndex) =>
           <tr key={`row-collection-${collectionIndex}`}>
             <th key={`row-collection-${collectionIndex}`}>{props.allCollections[collectionIndex].code ?? UNASSIGNED_COLLECTION }</th>
-            {Object.keys(props.allSizes).map((sizeIndex) => (
+            { Object.keys(props.allSizes).map((sizeIndex) => (
               <td key={`cell-${collectionIndex}-${sizeIndex}`}
                   style={{
                     textAlign: "right",
